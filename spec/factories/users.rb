71628a65b1588e45ba1fb7b965c71a4690a5ad41
totti-ue <osamu232@yahoo.co.jp>
2020-03-09 19:08:33 +0900

    oparated_test_for_user_model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,18 @@
 FactoryBot.define do
   factory :user do
     password = Faker::Internet.password(min_length: 6)
-    nickname               {Faker::JapaneseMedia::DragonBall.character}
-    email                  {Faker::Internet.email}
-    password               {password}
-    password_confirmation  {password}
-    family_name            {Faker::Name.last_name}
-    family_name_kana       {Faker::Name.last_name}
-    birthday               {Faker::Date.birthday}
-    avatar                 {"sample.png"}
-    profile                {"こんにちは。私はイトウです"}
-    phone_number           {Faker::PhoneNumber.cell_phone}
-    sex                    {"0"}
+    nickname                {Faker::JapaneseMedia::DragonBall.character}
+    email                   {Faker::Internet.email}
+    password                {password}
+    password_confirmation   {password}
+    last_name               {"安倍"}
+    first_name              {"晋三"}
+    last_name_kana          {"アベ"}
+    first_name_kana         {"シンゾウ"}
+    birthday                {Faker::Date.birthday}
+    avatar                  {"sample.png"}
+    profile                 {"こんにちは。私は安倍晋三です"}
+    phone_number            {Faker::PhoneNumber.cell_phone}
+    sex                     {"0"}
   end
 end
