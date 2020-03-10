58370381baaa802dd5dbb3a047c57e7c08e815e1
mrmtsu <81202muramatsu@gmail.com>
2020-03-10 19:58:26 +0900

    address_muramatsu
    # what
    ウィザード形式の実装。
    
    # why
    ユーザー管理に必要であるため。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-  def after_sign_in_path_for(resource)
-    root_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   root_path
+  # end
 
   def after_sign_out_path_for(resource)
     root_path
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :avatar, :profile, :phone_number, :sex])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :avatar, :profile, :sex])
     #binding.pry
   end
 
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,11 @@
 class Address < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :last_name, presence: true, format: { with: /[ぁ-んァ-ンａ-ｚＡ-Ｚ一-龥]+/ }
   validates :first_name, presence: true, format: { with: /[ぁ-んァ-ンａ-ｚＡ-Ｚ一-龥]+/ }
   validates :last_name_kana, presence: true, format: { with: /[ァ-ン]+/ }
   validates :first_name_kana, presence: true, format: { with: /[ァ-ン]+/ }
-  validates :phone_number, presence: true, format: { with: /0\d{1,4}-*\d{1,4}-*\d{4}/ }
+  validates :phone_number, presence: true, format: { with: /0\d{1,4}\-*\d{1,4}\-*\d{4}/ }
   validates :post_code, presence: true, format: { with: /\d{3}\-*\d{4}/ }
   validates :prefecture, presence: true
   validates :city, presence: true
