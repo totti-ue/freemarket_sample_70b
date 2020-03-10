e64e70c23b62f0e049cfa738d79b9a330c431844
mrmtsu <81202muramatsu@gmail.com>
2020-03-10 12:13:08 +0900

    newlogin_second

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
 
   def create
-    params[:user][:birthday] = birthday_join
+    params[:user][:birthday] 
     @user = User.new(user_params)
   end
 
@@ -16,18 +16,5 @@ class UsersController < ApplicationController
    def user_params
      params.require(:user).permit(:birthday)
    end
- 
-   def birthday_join
-     # パラメータ取得
-     date = params[:user][:birthday]
- 
-     # ブランク時のエラー回避のため、ブランクだったら何もしない
-     if date["birthday(1i)"].empty? && date["birthday(2i)"].empty? && date["birthday(3i)"].empty?
-       return
-     end
- 
-     # 年月日別々できたものを結合して新しいDate型変数を作って返す
-     Date.new date["birthday(1i)"].to_i,date["birthday(2i)"].to_i,date["birthday(3i)"].to_i
- 
-   end
+
 end
