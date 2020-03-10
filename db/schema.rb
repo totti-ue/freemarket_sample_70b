a791c4ada1e880d287d8e62eb6aa36a5655e7314
tomotomo0511 <tomokaru0511@gmail.com>
2020-03-10 21:47:59 +0900

    item_db:migrate2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_054246) do
+ActiveRecord::Schema.define(version: 2020_03_10_070030) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -27,6 +27,30 @@ ActiveRecord::Schema.define(version: 2020_03_10_054246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.bigint "category_id", null: false
+    t.bigint "brand_id"
+    t.integer "status", null: false
+    t.string "delivery_chage", null: false
+    t.string "delivery_origin", null: false
+    t.string "delivery_size", null: false
+    t.string "delivery_method", null: false
+    t.string "delivery_days", null: false
+    t.integer "price", null: false
+    t.bigint "history_id"
+    t.bigint "saler_id"
+    t.bigint "buyer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_items_on_brand_id"
+    t.index ["buyer_id"], name: "index_items_on_buyer_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["history_id"], name: "index_items_on_history_id"
+    t.index ["saler_id"], name: "index_items_on_saler_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
