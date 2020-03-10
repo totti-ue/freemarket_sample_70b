6cb9be5dffc68900ad71e9514470ad8461212e88
mrmtsu <81202muramatsu@gmail.com>
2020-03-10 11:58:55 +0900

    new_login_muramatsu
    #what
    ・購入画面とアイコンの色など統一
    ・生年月日の登録
    
    #why
    ・アプリの統一感を出す
    ・ユーザー管理で必要

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
 
-  def show
+  def new
   end
 
+
+  def create
+    params[:user][:birthday] = birthday_join
+    @user = User.new(user_params)
+  end
+
+  def show
+  end
+  
+ private 
+   def user_params
+     params.require(:user).permit(:birthday)
+   end
+ 
+   def birthday_join
+     # パラメータ取得
+     date = params[:user][:birthday]
+ 
+     # ブランク時のエラー回避のため、ブランクだったら何もしない
+     if date["birthday(1i)"].empty? && date["birthday(2i)"].empty? && date["birthday(3i)"].empty?
+       return
+     end
+ 
+     # 年月日別々できたものを結合して新しいDate型変数を作って返す
+     Date.new date["birthday(1i)"].to_i,date["birthday(2i)"].to_i,date["birthday(3i)"].to_i
+ 
+   end
 end
