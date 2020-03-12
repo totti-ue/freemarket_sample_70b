6a6fd8cf8d21a2195553934e61f544931767bc94
SuzukiTomoya12 <a1220suzuki@gmail.com>
2020-03-12 12:18:43 +0900

    create_image_table
    # what
    ・imageテーブル作成
    ・エラーの出るpayjpの記述をコメントアウト
    ・ログイン前に出品できないように、調整
    ・gemのインストール
    
    # why
    出品機能のサーバーサイド作業のため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_110459) do
+ActiveRecord::Schema.define(version: 2020_03_12_024406) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 2020_03_11_110459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -85,4 +93,5 @@ ActiveRecord::Schema.define(version: 2020_03_11_110459) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "images", "items"
 end
