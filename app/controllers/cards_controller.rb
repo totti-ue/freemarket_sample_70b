b012e47a55ae082dd567765e5b67af1c8af0b23b
totti-ue <osamu232@yahoo.co.jp>
2020-03-19 18:45:01 +0900

    refactoring_cards_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,13 +4,13 @@ class CardsController < ApplicationController
 
   def new
     card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    redirect_to user_card_path(curernt_user) if card.exists?
   end
 
   def pay
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     if params['payjp-token'].blank?
-      redirect_to action: "new"
+      redirect_to new_user_card(current_user)
     else
       customer = Payjp::Customer.create(
       description: '登録テスト',
@@ -20,29 +20,28 @@ class CardsController < ApplicationController
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "show"
+        redirect_to user_card_path(curernt_user)
       else
-        redirect_to action: "pay"
+        redirect_to pay_user_cards_path(current_user)
       end
     end
   end
 
   def delete
     card = Card.where(user_id: current_user.id).first
-    if card.blank?
-    else
+    if card.present?
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.delete
     end
-      redirect_to action: "new"
+      redirect_to new_user_card(current_user)
   end
 
   def show
     card = Card.where(user_id: current_user.id).first
     if card.blank?
-      redirect_to action: "new" 
+      redirect_to new_user_card(current_user)
     else
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       customer = Payjp::Customer.retrieve(card.customer_id)
