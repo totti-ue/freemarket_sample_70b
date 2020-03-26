ec67bcb12f73eaca7133441c19aa54e0e7625d6b
tomotomo0511 <tomokaru0511@gmail.com>
2020-03-26 16:23:49 +0900

    card-register-page-images-edit
    # what
    入力内容（先頭の数字かつ桁数）で表示するカードを絞るようにした
    
    # why
    ユーザーが自分の登録するカードをわかりやすくするため

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -31,3 +31,46 @@ document.addEventListener(
   },
   false
 );
+
+$(function () {
+  searchImage = function(){
+
+    $('.card-form').on('keyup',function() {
+      targetText = $(this).val();
+      console.log(targetText)
+      // 検索対象となるリストに入力された文字列が存在するかどうかを判断
+      if (targetText.slice(0,1) == 4 && String(targetText).length == 16) {
+        console.log("visa");
+        //.emptyで小要素を削除
+        $('.signup-card-list').empty()
+        //.appendで対応する画像のみ追加
+        $('.signup-card-list').append(`<img src="/assets/logo/visa.png" width="60px" height="40px" class="card-list-image">`)
+      } else if (targetText.slice(0,1) == 5 && String(targetText).length == 16) {
+        console.log("master");
+        $('.signup-card-list').empty()
+        $('.signup-card-list').append(`<img src="/assets/logo/master.png" width="60px" height="40px" class="card-list-image">`)
+      }   else if (targetText.slice(0,1) == 3 && String(targetText).length == 15) {
+        console.log("amex");
+        $('.signup-card-list').empty()
+        $('.signup-card-list').append(`<img src="/assets/logo/amex.png" width="60px" height="40px" class="card-list-image">`)
+      } else if (targetText.slice(0,1) == 3 && String(targetText).length == 14) {
+        console.log("diners");
+        $('.signup-card-list').empty()
+        $('.signup-card-list').append(`<img src="/assets/logo/diners_club.png" width="60px" height="40px" class="card-list-image">`)
+      } else if (targetText.slice(0,1) == 3 && String(targetText).length == 16) {
+        console.log("jcb");
+        $('.signup-card-list').empty()
+        $('.signup-card-list').append(`<img src="/assets/logo/jcb.png" width="60px" height="40px" class="card-list-image">`)
+      } else if (targetText.slice(0,1) == 6 && String(targetText).length == 16) {
+        console.log("discover");
+        $('.signup-card-list').empty()
+        $('.signup-card-list').append(`<img src="/assets/logo/discover.png" width="60px" height="40px" class="card-list-image">`)
+      } else {
+        console.log("失敗");
+      }
+    });
+  };
+
+  // searchImageの実行
+  $('.card-form').on('input', searchImage);
+});
